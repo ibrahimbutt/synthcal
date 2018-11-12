065ff07eb7bbc45f29bdfccc054f7f020c7f1132
Ibrahim Butt <ibrahimbutt@me.com>
2018-11-12 14:51:11 +0000

    Format (installed Prettier extention on VSCode)

--- main.js
+++ main.js
@@ -1,40 +1,41 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path');
-const url = require('url');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const url = require("url");
 
 if (process.env.ELECTRON_START_URL) {
-  require('electron-reload')(__dirname)
+  require("electron-reload")(__dirname);
 }
 
-let window
+let window;
 
 const createWindow = () => {
-  let window = new BrowserWindow({ width: 800, height: 600 })
+  let window = new BrowserWindow({ width: 800, height: 600 });
 
-  const startUrl = process.env.ELECTRON_START_URL || url.format({
-    pathname: path.join(__dirname, './build/index.html'),
-    protocol: 'file:',
-    slashes: true
+  const startUrl =
+    process.env.ELECTRON_START_URL ||
+    url.format({
+      pathname: path.join(__dirname, "./build/index.html"),
+      protocol: "file:",
+      slashes: true
+    });
+
+  window.loadURL(startUrl);
+
+  window.on("closed", function() {
+    window = null;
   });
+};
 
-  window.loadURL(startUrl)
+app.on("ready", createWindow);
 
-  window.on('closed', function () {
-    window = null
-  })
-
-}
-
-app.on('ready', createWindow)
-
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on("window-all-closed", function() {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
 });
 
-app.on('activate', function () {
+app.on("activate", function() {
   if (window === null) {
-    createWindow()
+    createWindow();
   }
 });
