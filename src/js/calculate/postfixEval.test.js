0a15db9a10c8101e1c478b67082f3774db14d882
Ibrahim Butt <ibrahimbutt@me.com>
2018-11-15 15:31:03 +0000

    Test postfix evaluation function

---
+++ src/js/calculate/postfixEval.test.js
@@ -0,0 +1,16 @@
+let postfixEval = require("./postfixEval");
+
+describe(`postfixEval:`, () => {
+  test(`postfixEval(tokens) should return 173`, () => {
+    const tokens = ["5", "6", "3", "/", "84", "*", "+"];
+    expect(postfixEval(tokens)).toEqual(173);
+  });
+  test(`postfixEval(tokens) should return 2`, () => {
+    const tokens = ["1", "1", "+"];
+    expect(postfixEval(tokens)).toEqual(2);
+  });
+  test(`postfixEval(tokens) should return 12`, () => {
+    const tokens = ["11", "1", "+"];
+    expect(postfixEval(tokens)).toEqual(12);
+  });
+});
