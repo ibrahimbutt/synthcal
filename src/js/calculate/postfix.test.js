d6bf9de3e57a52941988f02c3219c08d366a85c3
Ibrahim Butt <ibrahimbutt@me.com>
2018-11-15 15:28:54 +0000

    Test infix to postfix

---
+++ src/js/calculate/postfix.test.js
@@ -0,0 +1,16 @@
+let toPostfix = require("./postfix");
+
+describe(`toPostfix:`, () => {
+  test(`toPostfix(tokens) should return ['5', '6', '3', '/', '84', '*', '+']`, () => {
+    const tokens = ["5", "+", "6", "/", "3", "*", "84"];
+    expect(toPostfix(tokens)).toEqual(["5", "6", "3", "/", "84", "*", "+"]);
+  });
+  test(`toPostfix(tokens) should return ['1, '1', '+']`, () => {
+    const tokens = ["1", "+", "1"];
+    expect(toPostfix(tokens)).toEqual(["1", "1", "+"]);
+  });
+  test(`toPostfix(tokens) should return ['11, '1', '+']`, () => {
+    const tokens = ["11", "+", "1"];
+    expect(toPostfix(tokens)).toEqual(["11", "1", "+"]);
+  });
+});
