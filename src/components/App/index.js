8d510eb95e35a77cf6eadb5cd6adc8669e0f3797
Ibrahim Butt <ibrahimbutt@me.com>
2018-11-17 10:01:17 +0000

    Style App/index.js

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,3 +1,32 @@
 import React from "react";
+import styled from "styled-components";
 
-export default () => <h1>Hello World!</h1>;
+import Display from "../Display";
+import Buttons from "../Buttons";
+
+
+class App extends React.Component {
+  state = {
+    display: 458
+  };
+
+  render() {
+    return (
+      <div className={this.props.className}>
+        <Display content={this.state.display} />
+        <Buttons />
+      </div>
+    );
+  }
+}
+
+export default styled(App)`
+  background: linear-gradient(
+      180deg,
+      rgba(205, 237, 229, 0.05) 50%,
+      transparent 0
+    ),
+    linear-gradient(#84e, #004cee);
+  background-size: 4px 4px, 100% 100%;
+  height: 100vh;
+`;
