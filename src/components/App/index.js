fe04b44e11325b3c66ccc2c01aa35e867f97359c
Ibrahim Butt <ibrahimbutt@me.com>
2018-11-23 00:19:04 +0000

    Create onNumberPress function

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -27,6 +27,27 @@ class App extends React.Component {
     });
   };
 
+  onNumberPress = button => {
+    if (button === "0" && this.state.display === "0") {
+      return false;
+    } else if (
+      this.removeFormatting(this.state.display.length) > 10 &&
+      !this.state.operatorPressedLast
+    ) {
+      return false;
+    } else if (this.state.display === "0" || this.state.operatorPressedLast) {
+      this.setState({ display: button, operatorPressedLast: false });
+    } else {
+      let display = this.removeFormatting(this.state.display) + button;
+      this.setState({
+        display:
+          display > 999999999
+            ? Big(display).toExponential(2)
+            : Number(display).toLocaleString("en")
+      });
+    }
+  };
+
   };
 
   render() {
